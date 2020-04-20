07dd0e0b7a1462331a8c699cfc838e85d5cad3f2
MockinU <vlad.mocanu13@gmail.com>
2020-04-20 16:46:21 +0300

    1.0TestingGrounds

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGrounds.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+{
+    auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+    auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+    return EBTNodeResult::Succeeded;  
+}
